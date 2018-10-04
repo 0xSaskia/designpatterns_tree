dea96d84107e5a152f682220bb4e8cb50684cbf0
Saskia <saskiadegilder@gmail.com>
2018-10-04 13:51:23 +0200

    BAM

---
+++ BalancedBinaryTree.Tests/BBTreeTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using FluentAssertions;
+
+namespace BalancedBinaryTree.Tests
+{
+    public class BBTreeTests
+    {
+        [Fact]
+        public void Ctor_NoArguments_ShouldCreateEmptyTree()
+        {
+            var sut = new BBTree<int>();
+
+            sut.Contains(5).Should().BeFalse();
+
+        }
+
+        [Fact]
+        public void Contains_OneElementAdded_ReturnsTrue()
+        {
+            var sut = new BBTree<int>();
+
+            sut.Add(5);
+
+            sut.Contains(5).Should().BeTrue();
+        }
+
+        [Fact]
+        public void CumulativeAddingElementShouldContainThatElement()
+        {
+            var sut = new BBTree<int>();
+            var elements = new List<int>
+            {
+                5, 15, -5, default(int), int.MaxValue, 3
+            };
+
+            elements.ForEach( element => sut.Add(element));
+
+            elements.ForEach(element => sut.Contains(element).Should().BeTrue());}
+        }
+}
---
+++ BalancedBinaryTree/BBTree.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace BalancedBinaryTree
+{
+    public class BBTree<T> where T : IComparable<T>
+    {
+        private Node root = null;
+
+        public bool Contains(T value)
+        {
+            return FindNodeWithValue(root, value) != null;
+        }
+
+        public void Add(T value)
+        {
+            Add(value, ref root);
+        }
+
+        private void Add(T value, ref Node at)
+        {
+            if (at == null)
+            {
+                at = new Node {Value = value};
+            }
+            else if (value.IsSmallerThan(at.Value))
+            {
+                var left = at.Left;
+                Add(value, ref left);
+                at.Left = left;
+            }
+            else if (value.IsLargerThan(at.Value))
+            {
+                var right = at.Right;
+                Add(value, ref right);
+                at.Right = right;
+            }
+        }
+
+        internal Node FindNodeWithValue(Node at, T value)
+        {
+            if (at.Value.Equals(value)) return at;
+            if (value.IsSmallerThan(at.Value))
+            {
+                return FindNodeWithValue(at.Left, value);
+            }
+            else
+            {
+                return FindNodeWithValue(at.Right, value);
+            }
+        }
+
+        internal class Node
+        {
+            public T Value { get; set; }
+            public Node Left;
+            public Node Right;
+            public int Height { get; set; } = 1;
+
+            private void UpdateNodeHeight(Node root)
+            {
+                root.Height = Math.Max(Left?.Height ?? 0, Right?.Height ?? 0) + 1;
+            }
+        }
+    }
+
+    internal static class NodeExtensions
+    {
+        public static bool IsSmallerThan<T>(this T left, T right) where T : IComparable<T>
+            => left.CompareTo(right) < 0;
+
+        public static bool IsLargerThan<T>(this T left, T right)
+            where T : IComparable<T>
+            => left.CompareTo(right) > 0;
+    }
+}
